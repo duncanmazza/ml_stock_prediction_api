a1e6b2b8408b2e73a6627abfe98ad4faa5c85786
Duncan Mazza <dmazza@olin.edu>
2019-11-16 11:59:54 -0500

    Add description to AV_API_KEY variable

--- src/get_data/config.py
+++ src/get_data/config.py
@@ -4,4 +4,5 @@ This file stores sensitive API keys for acquiring data - DO NOT UPLOAD THE API K
 @author: Duncan Mazza
 """
 
+# Alpha-Vantage api key; get one here for free: https://www.alphavantage.co/
 AV_API_KEY = "YOUR_KEY_HERE"
